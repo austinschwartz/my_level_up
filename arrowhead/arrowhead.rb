084ea28a2d94f4962e0882b04d77c41c7edefa52
Austin Schwartz <aschwartz@clm-aschwartz.enova.com>
2015-06-09 15:55:17 -0500

    Arrowhead exercise

---
+++ arrowhead/arrowhead.rb
@@ -0,0 +1,30 @@
+class Arrowhead
+  # This seriously belongs in a database.
+  CLASSIFICATIONS = {
+    far_west: {
+      notched: "Archaic Side Notch",
+      stemmed: "Archaic Stemmed",
+      lanceolate: "Agate Basin",
+      bifurcated: "Cody",
+    },
+    northern_plains: {
+      notched: "Besant",
+      stemmed: "Archaic Stemmed",
+      lanceolate: "Humboldt Constricted Base",
+      bifurcated: "Oxbow",
+    },
+  }
+
+  # FIXME: I don't have time to deal with this.
+  def self.classify(region, shape)
+    shapes = CLASSIFICATIONS[region]
+    raise "Unknown region, please provide a valid region." if shapes.nil?
+
+    arrowhead = shapes[shape]
+    raise "Unknown shape value. Are you sure you know what you're talking about?" if arrowhead.nil? 
+
+    print "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
+  end
+end
+
+puts Arrowhead.classify(:northern_plains, :bifurcated)
