3ec4c66f3d0d5bad054c883fa381e1e1cec6c77a
Austin Schwartz <aschwartz@clm-aschwartz.enova.com>
2015-06-11 14:29:00 -0500

    changes to dinos:

--- dino_catalog/dinodex.rb
+++ dino_catalog/dinodex.rb
@@ -3,28 +3,78 @@ require 'CSV'
 class DinoDex
   BIG_SIZE = (4000..Float::INFINITY)
   SMALL_SIZE = (1..4000)
+  KEY_MAPPING = {
+    name: :genus,
+    period: :period,
+    carnivore: :carnivore,
+    weight_in_lbs: :weight,
+    walking: :walking,
+    diet: :diet
+  }
 
-  def initialize(*csv_files)
-    csv_files = ['dinodex.csv', 'african_dinosaur_export.csv'] if csv_files.empty?
+  def initialize(*files)
+    files = ['dinodex.csv', 'african_dinosaur_export.csv'] if files.empty?
     @csvs = []
-    csv_files.each do |file|
-      @csvs << CSV.read(file, headers: true, header_converters: :symbol, converters: :all)
+    files.each do |file|
+      @csvs << CSV.read(file, headers: true, header_converters: :symbol,
+                              converters: :all)
     end
   end
 
-  def query(query_hash)
-    lowercase_hash = query_hash.map { |k, v| { k.downcase => v } }.reduce(:merge)
+  def clear
+    initialize
+    self
+  end
+
+  def filter(filter_hash)
     @csvs.each do |csv|
-      if csv.headers[0] == :name
-        query_master_csv(csv, lowercase_hash)
-      else
-        query_african_csv(csv, lowercase_hash)
-      end
+      new_hash = convert_header_keys(csv, filter_hash)
+      filter_csv(csv, new_hash)
     end
     self
   end
 
-  def diet_matches(row, value)
+  def filter_csv(csv, filter_hash)
+    csv.delete_if do |row|
+      matches = true
+      filter_hash.each do |k, v|
+        unless row_matches_filter?(row, k, v)
+          matches = false
+          break
+        end
+      end
+      !matches
+    end
+  end
+
+  def convert_header_keys(csv, filter_hash)
+    lowercase_hash = filter_hash.map { |k, v| Hash[k.downcase, v] }.reduce(:merge)
+    if csv.headers.include? :genus
+      lowercase_hash.map { |k, v| Hash[KEY_MAPPING[k], v] }.reduce(:merge)
+    else
+      lowercase_hash
+    end
+  end
+
+  def string_matches?(row, k, v)
+    if k == :diet
+      return false unless diet_matches?(row, v)
+    elsif row.key?(k) && !row[k].include?(v)
+      return false
+    end
+    true
+  end
+
+  def row_matches_filter?(row, k, v)
+    if v.is_a? String
+      return string_matches?(row, k, v)
+    else
+      return false unless v.include?(row[k].to_i)
+    end
+    true
+  end
+
+  def diet_matches?(row, value)
     if row.key? :diet
       return row[:diet].include? value
     else
@@ -34,54 +84,19 @@ class DinoDex
     false
   end
 
-  def query_master_csv(csv, query_hash)
-    csv.delete_if do |row|
-      matches = true
-      query_hash.each do |k, v|
-        if v.is_a? String
-          if k == :diet && !diet_matches(row, v)
-            matches = false
-            break
-          elsif row.key?(k) && !row[k].include?(v)
-            matches = false
-            break
-          end
-        elsif v.is_a? Range
-          matches = false unless v.include?(row[k].to_i)
-          break
-        end
-      end
-      !matches
+  def dino_fact(dino)
+    line = ["#{dino.headers[0].capitalize}: #{dino[dino.headers[0]]}"]
+    dino.each do |k, v|
+      line << " - #{k}: #{v}" unless v.nil? || k == dino.headers[0]
     end
-  end
-
-  def query_african_csv(csv, query_hash)
-    mapping = {
-      name: :genus,
-      period: :period,
-      carnivore: :carnivore,
-      weight_in_lbs: :weight,
-      walking: :walking,
-      diet: :diet,
-    }
-    new_hash = query_hash.map { |k, v| { mapping[k] => v } }.reduce(:merge)
-    query_master_csv(csv, new_hash)
-  end
-
-  def clear
-    initialize
-    self
+    line
   end
 
   def facts
     fact_list = []
     @csvs.each do |csv|
-      csv.each do |row|
-        str = ["#{row.headers[0].capitalize}: #{row[row.headers[0]]}"]
-        row.each do |k, v|
-          str << " - #{k}: #{v}" unless v.nil? || k == row.headers[0]
-        end
-        fact_list << str.join("\n")
+      csv.each do |dino|
+        fact_list << dino_fact(dino).join("\n")
       end
     end
     fact_list.join("\n\n")
@@ -98,23 +113,10 @@ class DinoDex
   end
 
   def to_json
-=begin
     @csvs
       .map { |csv| csv.map(&:to_hash) }
       .flatten
-      .map { |hash| hash.map { |k, v|  Hash[k.to_s.capitalize, v] }.reduce(:merge) }
+      .map { |h| h.map { |k, v| Hash[k.to_s.capitalize, v] }.reduce(:merge) }
       .to_s.gsub('=>', ': ').gsub('nil', '"NULL"')
-=end
-    @csvs.map do |csv|
-      csv.map(&:to_hash)
-    end.flatten.map do |hash|
-      hash.map do |k, v| 
-        Hash[k.to_s.capitalize, v]
-      end.reduce(:merge)
-    end.to_s.gsub('=>', ': ').gsub('nil', '"NULL"')
-  end
-
-  def to_s
-    to_names.to_s
   end
 end
--- dino_catalog/test_dinodex.rb
+++ dino_catalog/test_dinodex.rb
@@ -7,34 +7,34 @@ class TestDinoDex < Minitest::Test
   end
 
   def test_carnivores
-    assert_equal ["Afrovenator", "Albertosaurus", "Carcharodontosaurus", "Deinonychus", "Diplocaulus", "Giganotosaurus", "Megalosaurus", "Quetzalcoatlus", "Suchomimus", "Yangchuanosaurus"].sort, @dinodex.clear.query(Diet: "Carnivore").to_names
+    assert_equal ["Afrovenator", "Albertosaurus", "Carcharodontosaurus", "Deinonychus", "Diplocaulus", "Giganotosaurus", "Megalosaurus", "Quetzalcoatlus", "Suchomimus", "Yangchuanosaurus"].sort, @dinodex.clear.filter(Diet: "Carnivore").to_names
   end
 
   def test_chaining
-    assert_equal ["Diplocaulus", "Quetzalcoatlus"].sort, @dinodex.clear.query(Diet: "Carnivore").query(Walking: "Quadruped").to_names
+    assert_equal ["Diplocaulus", "Quetzalcoatlus"].sort, @dinodex.clear.filter(Diet: "Carnivore").filter(Walking: "Quadruped").to_names
   end
   
   def test_biped
-    assert_equal ["Abrictosaurus", "Afrovenator", "Albertonykus", "Albertosaurus", "Baryonyx", "Carcharodontosaurus", "Deinonychus", "Giganotosaurus", "Megalosaurus", "Suchomimus", "Yangchuanosaurus"].sort, @dinodex.clear.query(Walking: "Biped").to_names
+    assert_equal ["Abrictosaurus", "Afrovenator", "Albertonykus", "Albertosaurus", "Baryonyx", "Carcharodontosaurus", "Deinonychus", "Giganotosaurus", "Megalosaurus", "Suchomimus", "Yangchuanosaurus"].sort, @dinodex.clear.filter(Walking: "Biped").to_names
   end
 
   def test_period
-    assert_equal ["Albertonykus", "Albertosaurus", "Baryonyx", "Deinonychus", "Dracopelta", "Giganotosaurus", "Paralititan", "Quetzalcoatlus", "Suchomimus"].sort, @dinodex.clear.query(Period: "Cretaceous").to_names
+    assert_equal ["Albertonykus", "Albertosaurus", "Baryonyx", "Deinonychus", "Dracopelta", "Giganotosaurus", "Paralititan", "Quetzalcoatlus", "Suchomimus"].sort, @dinodex.clear.filter(Period: "Cretaceous").to_names
   end
 
   def test_big
-    assert_equal ["Baryonyx", "Giganotosaurus", "Giraffatitan", "Paralititan", "Suchomimus", "Yangchuanosaurus"].sort, @dinodex.clear.query(Weight_in_lbs: DinoDex::BIG_SIZE).to_names
+    assert_equal ["Baryonyx", "Giganotosaurus", "Giraffatitan", "Paralititan", "Suchomimus", "Yangchuanosaurus"].sort, @dinodex.clear.filter(Weight_in_lbs: DinoDex::BIG_SIZE).to_names
   end
 
   def test_small
-    assert_equal ["Abrictosaurus", "Albertosaurus", "Carcharodontosaurus", "Deinonychus", "Megalosaurus", "Melanorosaurus", "Quetzalcoatlus"].sort, @dinodex.clear.query(Weight_in_lbs: DinoDex::SMALL_SIZE).to_names
+    assert_equal ["Abrictosaurus", "Albertosaurus", "Carcharodontosaurus", "Deinonychus", "Megalosaurus", "Melanorosaurus", "Quetzalcoatlus"].sort, @dinodex.clear.filter(Weight_in_lbs: DinoDex::SMALL_SIZE).to_names
   end
 
   def test_json
-    assert_equal "[{\"Name\": \"Albertosaurus\", \"Period\": \"Late Cretaceous\", \"Continent\": \"North America\", \"Diet\": \"Carnivore\", \"Weight_in_lbs\": 2000, \"Walking\": \"Biped\", \"Description\": \"Like a T-Rex but smaller.\"}, {\"Name\": \"Megalosaurus\", \"Period\": \"Jurassic\", \"Continent\": \"Europe\", \"Diet\": \"Carnivore\", \"Weight_in_lbs\": 2200, \"Walking\": \"Biped\", \"Description\": \"Originally thought to be a Quadruped. First dinosaur to be named.\"}, {\"Genus\": \"Melanorosaurus\", \"Period\": \"Triassic\", \"Carnivore\": \"No\", \"Weight\": 2400, \"Walking\": \"Quadruped\"}]",@dinodex.clear.query(Weight_in_lbs: (525..2422)).to_json
+    assert_equal "[{\"Name\": \"Albertosaurus\", \"Period\": \"Late Cretaceous\", \"Continent\": \"North America\", \"Diet\": \"Carnivore\", \"Weight_in_lbs\": 2000, \"Walking\": \"Biped\", \"Description\": \"Like a T-Rex but smaller.\"}, {\"Name\": \"Megalosaurus\", \"Period\": \"Jurassic\", \"Continent\": \"Europe\", \"Diet\": \"Carnivore\", \"Weight_in_lbs\": 2200, \"Walking\": \"Biped\", \"Description\": \"Originally thought to be a Quadruped. First dinosaur to be named.\"}, {\"Genus\": \"Melanorosaurus\", \"Period\": \"Triassic\", \"Carnivore\": \"No\", \"Weight\": 2400, \"Walking\": \"Quadruped\"}]",@dinodex.clear.filter(Weight_in_lbs: (525..2422)).to_json
   end
 
   def test_facts
-    assert_equal "Name: Albertonykus\n - period: Early Cretaceous\n - continent: North America\n - diet: Insectivore\n - walking: Biped\n - description: Earliest known Alvarezsaurid.\n\nGenus: Paralititan\n - period: Cretaceous\n - carnivore: No\n - weight: 120000\n - walking: Quadruped", @dinodex.clear.query(Period: "Cretaceous").query(Diet: "Insectivore").facts
+    assert_equal "Name: Albertonykus\n - period: Early Cretaceous\n - continent: North America\n - diet: Insectivore\n - walking: Biped\n - description: Earliest known Alvarezsaurid.\n\nGenus: Paralititan\n - period: Cretaceous\n - carnivore: No\n - weight: 120000\n - walking: Quadruped", @dinodex.clear.filter(Period: "Cretaceous").filter(Diet: "Insectivore").facts
   end
 end
